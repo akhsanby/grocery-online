69546788c8f61b9c4b764e2286d1a92ecbdc1036
akhsanby <akhsanby@gmail.com>
2024-01-06 20:23:52 +0700

    update store redux

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "@/app/features/product-slice.js";
 import userReducer from "@/app/features/user-slice.js";
+import categoryReducer from "@/app/features/category-slice.js";
 import cartReducer from "@/app/features/cart-slice.js";
+import historyReducer from "@/app/features/history-slice.js";
 
 export const store = configureStore({
   reducer: {
     product: productReducer,
     user: userReducer,
+    category: categoryReducer,
     cart: cartReducer,
+    history: historyReducer,
   },
 });
